a46fa5fce1bc715500d40ce12ecb63fb4084277d
Brandon Michaud <michaudbrandond@gmail.com>
2024-03-07 18:46:52 -0600

    deep network full go

--- hw3.sh
+++ hw3.sh
@@ -6,12 +6,12 @@
 #SBATCH --mem=30G
 #SBATCH --output=outputs/hw3_%j_stdout.txt
 #SBATCH --error=outputs/hw3_%j_stderr.txt
-#SBATCH --time=01:00:00
+#SBATCH --time=03:00:00
 #SBATCH --job-name=hw3
 #SBATCH --mail-user=brandondmichaud@ou.edu
 #SBATCH --mail-type=ALL
 #SBATCH --chdir=/home/cs504319/cs5043-hw3
-#SBATCH --array=0
+#SBATCH --array=0-4
 
 . /home/fagg/tf_setup.sh
 conda activate dnn_2024_02
